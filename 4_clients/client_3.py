783f4f459253bca9e19ff42f0c1cb8a02b753286
Rafael Castillo Cebolla <rafaelcastillocebolla@gmail.com>
2025-02-05 11:55:48 +0100

    Add logger to clients 3 and 4

--- 4_clients/client_3.py
+++ 4_clients/client_3.py
@@ -6,4 +6,7 @@ CLIENT_NUMBER = 3
 if __name__ == "__main__":  
     excel_file_name = f"PI-CAI_3__part{CLIENT_NUMBER}.xlsx" 
     temp_csv_file_name = f"temp_database_{CLIENT_NUMBER}.csv"
-    start_flower_client(excel_file_name, temp_csv_file_name, context=None)
+    logger_name = f"client{CLIENT_NUMBER}.log"
+    
+    start_flower_client(excel_file_name, temp_csv_file_name, logger_name, context=None)
+    
