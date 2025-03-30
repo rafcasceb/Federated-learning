16518b978701b25b26abf120bfbce5e6551b1141
Rafael Castillo Cebolla <rafaelcastillocebolla@gmail.com>
2025-03-30 23:34:46 +0200

    Trying some more options

--- 4_clients/client_app.py
+++ 4_clients/client_app.py
@@ -1,5 +1,5 @@
-from collections import OrderedDict
 import os
+from collections import OrderedDict
 from typing import Any, Dict, List, Tuple
 
 import numpy as np
@@ -7,6 +7,7 @@ import pandas as pd
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.nn.init as init
 import torch.optim as optim
 from flwr.client import ClientApp, NumPyClient, start_client
 from flwr.common import Context
@@ -93,11 +94,25 @@ class NeuralNetwork(nn.Module):
         
         layers = []
         layers.append(nn.Linear(input_size, hidden_sizes[0]))
+        
+        init.kaiming_uniform_(layers[-1].weight, nonlinearity='relu')
+        layers.append(nn.BatchNorm1d(hidden_sizes[0]))  
+        layers.append(nn.ReLU())
+        layers.append(nn.Dropout(0.1))  
+
         for in_size, out_size in zip(hidden_sizes[:-1], hidden_sizes[1:]):
             layers.append(nn.Linear(in_size, out_size))
+            
+            init.kaiming_uniform_(layers[-1].weight, nonlinearity='relu')
+            #init.xavier_uniform_(layers[-1].weight)
+            layers.append(nn.BatchNorm1d(out_size))
+            #layers.append(nn.LeakyReLU())
             layers.append(nn.ReLU())
+            layers.append(nn.Dropout(0.1))
+
         layers.append(nn.Linear(hidden_sizes[-1], output_size))
-        
+        init.xavier_uniform_(layers[-1].weight)
+
         self.net = nn.Sequential(*layers)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
