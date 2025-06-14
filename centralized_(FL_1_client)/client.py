594c24eb892222be527fc4b821d61399c61bd67f
Rafael Castillo Cebolla <rafaelcastillocebolla@gmail.com>
2025-06-14 10:45:36 +0200

    chore: removed old centralized implementations

--- centralized_(FL_1_client)/client.py
+++
@@ -1,6 +0,0 @@
-from client_app import start_flower_client
-
-if __name__ == "__main__":  
-    excel_file_name = "PI-CAI_3.xlsx" 
-    temp_csv_file_name = "temp_database.csv"
-    start_flower_client(excel_file_name, temp_csv_file_name, context=None)
